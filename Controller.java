45ac5057a0011a198076f78ad5d241463ec1fe79
Drew Miller <drew.miller@mrjaffesclass.com>
2024-02-08 14:25:01 -0800

    start and stop 3, need to keep track of wins for player1 and 2

--- Controller.java
+++ Controller.java
@@ -11,49 +11,40 @@ public class Controller
     private Board board;
     private String player1;
     private String player2;
-
-    public boolean eventLoop() {
+    
+    public void start() {
         //instantiate variables
         scanner = new Scanner(System.in);
         this.board = new Board();
-
-        //request to leave or continue
-        while(true) {
-            System.out.println(Constants.SS);
-            String in = scanner.nextLine();
-            if (in.equals(Constants.START)) {
-                break;
-            }
-            if (in.equals(Constants.STOP)) {
-                return false;
-            }
-        } 
-
 
         //request player names
         System.out.printf(Constants.REQUEST_PLAYER_NAME, 1);
         player1 = scanner.nextLine();
         System.out.printf(Constants.REQUEST_PLAYER_NAME, 2);
         player2 = scanner.nextLine();
-
+        
+        //request to leave or continue
+        while(true) {
+            System.out.println(Constants.SS);
+            String in = scanner.nextLine();
+            if (in.equals(Constants.START)) {
+                eventLoop();
+            }
+            if (in.equals(Constants.STOP)) {
+                return;
+            }
+        } 
+    }
+    
+    public void eventLoop() {
         //run event loop
         System.out.printf(Constants.RULES+"\n",player1, player2);
-        board.printBoard();
         //add while loop here
-        
-        // go back to controller and start a new game
-        return true;
+        board.printBoard();
     }
 
     public static void main(String[] args) {
-        boolean gameIsRunning = true;
-        
-        while (gameIsRunning) {
-            Controller controller = new Controller();
-            boolean newGame = controller.eventLoop();
-            if (!newGame) {
-                gameIsRunning = false;
-            }
-        }
+        Controller controller = new Controller();
+        controller.start();
     }
 }
